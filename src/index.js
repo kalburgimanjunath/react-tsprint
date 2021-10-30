39c38159b38c0599d58077af2b29de0b9c37b554
kalburgimanjunath <manjunathkalburgi65512@gmail.com>
2021-10-30 19:46:05 +0530

    4 files modified, 1 added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import App from "./App";
-
-ReactDOM.render(<App />, document.getElementById("root"));
+import App from './App';
+import './libs/sb-admin-2.min.js';
+ReactDOM.render(<App />, document.getElementById('root'));
